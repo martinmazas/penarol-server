dcaf1ece207868bf47b6af9600e12f60a24befa4
martinmazas <mazasmartin@gmail.com>
2021-09-19 15:03:21 +0300

    .

--- db_connection.js
+++ db_connection.js
@@ -1,24 +1,3 @@
-// const { MongoClient } = require('mongodb');
-// async function main() {
-//     const uri = 'mongodb+srv://martinmazas:mm401602@cluster0.f2wkg.mongodb.net/penarol-app?retryWrites=true&w=majority';
-//     const client = new MongoClient(uri);
-//     try {
-//         await client.connect();
-//         await listDatabases(client);
-//     } catch (e) {
-//         console.error(e);
-//     } finally {
-//         await client.close();
-//     }
-// }
-// main().catch(console.error);
-
-// async function listDatabases(client) {
-//     const databasesList = await client.db().admin().listDatabases();
-
-//     databasesList.databases.forEach(db => { console.log(db.name); })
-// }
-
 const mongoose = require('mongoose');
 const consts = require('./constants');
 
