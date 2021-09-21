27991b6539dc8d63a40411b3d37c45f68d79bb41
martinmazas <mazasmartin@gmail.com>
2021-09-21 11:29:31 +0300

    Update playerRouter.js

--- Routers/playerRouter.js
+++ Routers/playerRouter.js
@@ -3,9 +3,7 @@ const { playerDBController } = require('../controllers/player.ctrl');
 const playerRouter = new Router();
 
 playerRouter.get('/', playerDBController.getPlayer);
-// userRouter.get('/:id', userDBController.getUser);
 playerRouter.post('/', playerDBController.addPlayer);
-// userRouter.post('/:email', userDBController.login);
 // userRouter.put('/:id', userDBController.updateUserOrAddToFavorites);
 playerRouter.delete('/:id', playerDBController.removePlayer);
 
